61b71d948cc079cf3a11cc26c7793ebd0e2dbfa2
sudarakas <sudharakafb@gmail.com>
2019-04-25 19:58:56 +0530

    update

--- app/House.php
+++ app/House.php
@@ -9,6 +9,7 @@ class House extends Model
     protected $fillable = [
         'noOfRooms', 'noOfKitchen', 'noOfFloors','noOfWashrooms','size','swimmingPool','garden','nearestSchool','nearestRailway','nearestBusStop'
     ];
+    
     public function property(){
 
         return $this->belongsTo(Property::class);
--- app/Land.php
+++ app/Land.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Land extends Model
 {
-    //
+    protected $fillable = [
+        'noOfRooms', 'noOfKitchen', 'noOfFloors','noOfWashrooms','size','swimmingPool','garden','nearestSchool','nearestRailway','nearestBusStop'
+    ];
+    public function property(){
+
+        return $this->belongsTo(Property::class);
+
+    }
 }

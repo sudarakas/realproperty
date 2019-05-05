41656eba6a57c5bc41ba422e53bb9d68d5462c8d
sudarakas <sudharakafb@gmail.com>
2019-05-05 06:12:21 +0530

    modal ui update

--- resources/views/layouts/offerwarehouse.blade.php
+++ resources/views/layouts/offerwarehouse.blade.php
@@ -1,58 +1,64 @@
-<div id="myModal" class="modal column is-half is-offset-one-quarter">
-  <div class="modal-content">
-    <div class="is-pulled-right">
-      <span class="close has-text-danger"><i class="far fa-times-circle"></i></span>
-    </div>
-    <p class="title is-6 has-text-centered">Submit your offer</p>
-    <p class="subtitle is-7 has-text-danger has-text-centered">Important : Your offer should be higher than current offer</p>
-    <form action="/warehouse/{{$warehouse->id}}/offer" method="post">
-      @csrf
-      <div class="field">
-        <div class="control column is-8 is-offset-2">
-          <input class="input is-7" name="offeramount" type="text" placeholder="Enter Your Offer Amount">
-          <input name="propertyid" type="text" value="{{$warehouse->property_id}}" hidden>
-          <input name="warehouseid" type="text" value="{{$warehouse->id}}" hidden>
-        </div>
-      </div>
-      <div class="field is-centered has-text-centered">
-        <button type="submit" class="button is-info"><span class="savebutton">Submit</span></button>
-      </div>
-    </form>
+<div class="modal off" id="offer">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+      <header class="modal-card-head">
+          <p class="modal-card-title has-text-centered">Submit your offer</p>
+          <button class="delete closeoffer" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+      <p class="subtitle is-6 has-text-centered"><strong class="has-text-danger">Important : Your offer should be higher than current offer</strong></p>
+      <form action="/warehouse/{{$warehouse->id}}/offer" method="post">
+          @csrf
+          <div class="field">
+              <div class="control column is-8 is-offset-2">
+                  <input class="input is-7" name="offeramount" type="text" placeholder="Enter Your Offer Amount">
+                  <input name="propertyid" type="text" value="{{$warehouse->property_id}}" hidden>
+                  <input name="warehouseid" type="text" value="{{$warehouse->id}}" hidden>
+              </div>
+          </div>
+          <div class="field is-centered has-text-centered">
+              <button type="submit" class="button is-info"><span class="savebutton">Submit</span></button>
+          </div>
+      </form>
+      </section>
+      <footer class="modal-card-foot is-centered has-text-centered">
+        <p class="subtitle is-7 has-text-centered">If making any payments we recommend that you have two permanent & verified methods of contact of the payment receiver such as their landline number and home/business address.
+        </p>
+      </footer>
   </div>
-</div>
 </div>
 
 {{-- JS Script for popup --}}
 <script>
-  var modal = document.getElementById('myModal');
-            var btn = document.getElementById("myBtn");
-            var span = document.getElementsByClassName("close")[0];
-            btn.onclick = function() {
-              modal.style.display = "block";
-            }
-            span.onclick = function() {
-              modal.style.display = "none";
-            }
-            window.onclick = function(event) {
-              if (event.target == modal) {
-                modal.style.display = "none";
-              }
-            }
+  var modal = document.getElementById('offer');
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("closeoffer")[0];
+  btn.onclick = function() {
+      document.querySelector('.off').style.display = 'block';
+  }
+  span.onclick = function() {
+      document.querySelector('.off').style.display = 'none';
+  }
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          document.querySelector('.off').style.display = 'none';
+      }
+  }
 </script>
 {{-- Tablet View Submit --}}
 <script>
-  var modal = document.getElementById('myModal');
-            var btn = document.getElementById("myBtnM");
-            var span = document.getElementsByClassName("close")[0];
-            btn.onclick = function() {
-              modal.style.display = "block";
-            }
-            span.onclick = function() {
-              modal.style.display = "none";
-            }
-            window.onclick = function(event) {
-              if (event.target == modal) {
-                modal.style.display = "none";
-              }
-            }
+  var modal = document.getElementById('offer');
+  var btn = document.getElementById("myBtnM");
+  var span = document.getElementsByClassName("closeoffer")[0];
+  btn.onclick = function() {
+      document.querySelector('.off').style.display = 'block';
+  }
+  span.onclick = function() {
+      document.querySelector('.off').style.display = 'none';
+  }
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          document.querySelector('.off').style.display = 'none';
+      }
+  }
 </script>
--- resources/views/layouts/reporthouse.blade.php
+++ resources/views/layouts/reporthouse.blade.php
@@ -1,4 +1,4 @@
-<div id="reportModal" class="modal column is-half is-offset-one-quarter">
+{{-- <div id="reportModal" class="modal column is-half is-offset-one-quarter">
     <div class="modal-content">
         <div class="is-pulled-right">
             <span class="close closeme has-text-danger"><i class="far fa-times-circle"></i></span>
@@ -19,8 +19,37 @@
             </div>
         </form>
     </div>
-</div>
-</div>
+</div> --}}
+
+<div class="modal repo" id="reportModal">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Report Property</p>
+        <button class="delete closeme" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <form action="/house/{{$house->id}}/offer" method="post">
+            @csrf
+            <div class="field">
+                <div class="control column is-8 is-offset-2">
+                    <input class="input is-7" name="offeramount" type="text" placeholder="Enter Your Offer Amount">
+                    <input name="propertyid" type="text" value="{{$house->property_id}}" hidden>
+                    <input name="houseid" type="text" value="{{$house->id}}" hidden>
+                </div>
+            </div>
+            <div class="field is-centered has-text-centered">
+                <button type="submit" class="button is-info"><span class="savebutton">Submit</span></button>
+            </div>
+        </form>
+        </section>
+        <footer class="modal-card-foot is-centered has-text-centered">
+          <p class="subtitle is-7 has-text-centered">If making any payments we recommend that you have two permanent & verified methods of contact of the payment receiver such as their landline number and home/business address.
+          </p>
+        </footer>
+    </div>
+  </div>
+
 
 {{-- JS Script for popup --}}
 <script>
@@ -28,7 +57,7 @@
     var btn = document.getElementById("report");
     var span = document.getElementsByClassName("closeme")[0];
     btn.onclick = function() {
-      modal.style.display = "block";
+      document.querySelector('.repo').style.display = 'block';
     }
     span.onclick = function() {
       modal.style.display = "none";

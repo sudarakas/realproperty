9793b0cdf2ebf3a8dc87c1aac911888c808e31cf
sudarakas <sudharakafb@gmail.com>
2019-05-05 09:52:25 +0530

    apartmentContact Function added

--- app/Http/Controllers/UserEmailController.php
+++ app/Http/Controllers/UserEmailController.php
@@ -121,4 +121,41 @@ class UserEmailController extends Controller
         return back();
 
     }
+
+    public function apartmentContact(Request $request)
+    {
+ 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|email',
+            'pno' => 'required',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|max:2500|min:10'
+        ]);
+        
+       
+        if ($validator->fails()) {
+
+            Alert::error('Please check your inputs and correct the following errors', 'Invalid Attempt')->autoclose(3000);
+            return back()->withErrors($validator);
+        }
+
+        $owner = \App\User::find(request('owner'));
+
+        $message = new UserEmail;
+        $message->receiver_id = request('owner');
+        $message->senderMail = request('email');
+        $message->senderName = request('name');
+        $message->phoneNo = request('pno');
+        $message->subject = request('subject');
+        $message->message = request('message');
+        $message->save();
+
+        \Mail::to($owner->email)->send(new ContactMail($request));
+        
+        Alert::success('Message Sent', 'Your message has been sent successfully!')->autoclose(3000);
+
+        return back();
+
+    }
 }

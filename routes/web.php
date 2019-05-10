4956af952c96dbd2145d55c6d14460bf505102d8
sudarakas <sudharakafb@gmail.com>
2019-05-10 22:36:05 +0530

    admin deleteApartment function bug fixed

--- routes/web.php
+++ routes/web.php
@@ -110,8 +110,8 @@ Route::get('/profile/apartment/{apartment}/edit','ApartmentController@showEditAp
 Route::get('/admin/apartment/{apartment}/edit','AdminController@showAdminEditApartment')->middleware('auth:admin');
 Route::post('/admin/apartment/{apartment}/edit','ApartmentController@editApartment');
 Route::post('/profile/apartment/{apartment}/edit','ApartmentController@editApartment');
+Route::post('/profile/apartment/{apartment}/delete','ApartmentController@deleteApartment');
 Route::post('/admin/apartment/{apartment}/delete','ApartmentController@deleteLand')->middleware('auth:admin');
-
 
 Route::get('/warehouse/serach','PageController@warehousesearch');
 Route::get('/warehouse/{warehouse}','WarehouseController@viewWarehouse');

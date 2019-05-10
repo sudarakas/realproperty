351df54b2a230781bab18e8c90c6a5f39cd3b03e
sudarakas <sudharakafb@gmail.com>
2019-05-11 01:32:59 +0530

    Reset user password function added

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,96 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reset Your Password - RealProperty.lk</title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+    {{-- CSS Files --}}
+    <link rel="stylesheet" href="/css/bulma.min.css">
+    <link rel="stylesheet" href="/css/custom.css"> {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:300i,400,400i,500,500i,600|Kanit:300,300i,400,400i,500,500i,600"
+        rel="stylesheet">
+
+</head>
+
+<body class="grayme">
+
+    <div class="column is-full colorbar">
+        {{-- top color bar goes here --}}
+    </div>
+    <br>
+    <div class="columns fulllogin is-centered">
+        <div class="column is-two-thirds leftsideeffect">
+            <a href="/">
+                <figure class="image is-blacklogo">
+                    <img src="/img/logoblack.png" width="112" height="28">
+                </figure>
+            </a>
+            <div class="is-mobile textboxlogin">
+                <p class="subtitle has-text-dark is-6 has-text-centered">{{ ('Hi! User') }}</p>
+                <p class="title has-text-primary is-4 has-text-centered">{{ __('Reset Your Password') }}</p>
+            </div>
+            <div class="columns is-mobile is-centered">
+                <div class="column is-half">
+                    @if (session('resent'))
+                    <div class="notification is-success is-size-7">
+                        <button class="delete"></button> {{ __('A fresh verification link has been sent to your email address.')
+                        }} </div>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+            </div>
+            <div class="column is-8 is-offset-2">
+                    <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+    
+                            <div class="field">
+                                <label for="email" class="label">{{ __('E-Mail Address') }}</label>
+    
+                                <div class="control has-icons-left">
+                                    <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email" value="{{ old('email') }}" required>
+                                    <span class="icon is-small is-left">
+                                            <i class="fas fa-at"></i>
+                                          </span>
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong class="has-text-danger">{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+    
+                            <div class="field">
+                                <div class="control column is-offset-4 is-mobile">
+                                    <button type="submit" class="button is-primary">
+                                        {{ __('Request Reset Link') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+            </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+
+    {{-- Footer --}}
+    @include('layouts.footer') {{-- JavaScript Files --}}
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/fontawesome.js"></script>
+    <script src="/js/sweetalert.min.js"></script>
+    @include('sweet::alert')
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+                $notification = $delete.parentNode;
+                $delete.addEventListener('click', () => {
+                    $notification.parentNode.removeChild($notification);
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,122 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reset Your Password - RealProperty.lk</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+    {{-- CSS Files --}}
+    <link rel="stylesheet" href="/css/bulma.min.css">
+    <link rel="stylesheet" href="/css/custom.css"> {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:300i,400,400i,500,500i,600|Kanit:300,300i,400,400i,500,500i,600"
+        rel="stylesheet">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+</head>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+<body class="grayme">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="column is-full colorbar">
+        {{-- top color bar goes here --}}
+    </div>
+    <br>
+    <div class="columns fulllogin is-centered">
+        <div class="column is-two-thirds leftsideeffect">
+            <a href="/">
+                <figure class="image is-blacklogo">
+                    <img src="/img/logoblack.png" width="112" height="28">
+                </figure>
+            </a>
+            <div class="is-mobile textboxlogin">
+                <p class="subtitle has-text-dark is-6 has-text-centered">{{ ('Hi! User') }}</p>
+                <p class="title has-text-primary is-4 has-text-centered">{{ __('Reset Your Password') }}</p>
+            </div>
+            <div class="columns is-mobile is-centered">
+                <div class="column is-half">
+                    @if (session('resent'))
+                    <div class="notification is-success is-size-7">
+                        <button class="delete"></button> {{ __('A fresh verification link has been sent to your email address.')
+                        }} </div>
+                    @endif
                 </div>
+            </div>
+            <div class="column is-8 is-offset-2">
+                    <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
+        
+                            <div class="field">
+                                <label for="email" class="label">{{ __('E-Mail Address') }}</label>
+        
+                                <div class="control has-icons-left">
+                                    <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+                                    <span class="icon is-small is-left">
+                                            <i class="fas fa-at"></i>
+                                          </span>
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+        
+                            <div class="field">
+                                <label for="password" class="label">{{ __('Password') }}</label>
+        
+                                <div class="control has-icons-left">
+                                    <input id="password" type="password" class="input{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                    <span class="icon is-small is-left">
+                                            <i class="fas fa-lock"></i>
+                                          </span>
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+        
+                            <div class="field">
+                                <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
+        
+                                <div class="control has-icons-left">
+                                    <input id="password-confirm" type="password" class="input" name="password_confirmation" required>
+                                    <span class="icon is-small is-left">
+                                            <i class="fas fa-lock"></i>
+                                          </span>
+                                </div>
+                            </div>
+        
+                            <div class="field">
+                                <div class="column is-offset-4">
+                                    <button type="submit" class="button is-primary">
+                                        {{ __('Reset Password') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+            </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+
+    {{-- Footer --}}
+    @include('layouts.footer') {{-- JavaScript Files --}}
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/fontawesome.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+                $notification = $delete.parentNode;
+                $delete.addEventListener('click', () => {
+                    $notification.parentNode.removeChild($notification);
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>

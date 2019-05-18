1c4162c1f9af1c9eb1f5aa62b504260003989d03
sudarakas <sudharakafb@gmail.com>
2019-05-18 19:18:10 +0530

    FavoriteController Updated

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -18,134 +18,171 @@ class FavoriteController extends Controller
     {
         $this->middleware('auth');
     }
-    
-    public function favoriteHouse(House $house){
 
-        $favorite = new Favorite;
-        $favorite->property_id = $house->property->id;
-        $favorite->user_id = auth()->id();
-        $favorite->house_id = $house->id;
-        
+    public function favoriteHouse(House $house)
+    {
 
-        try{
-            $favorite->save();
-            Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
-            return back();
-        }catch(\Illuminate\Database\QueryException $e){
 
-            $errorCode = $e->errorInfo[1];
-            if($errorCode == '1062'){
-                Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+        $isExits = Favorite::where('property_id', '=', $house->property->id)
+            ->where('user_id', '=', auth()->id())
+            ->get();
+        if ($isExits->count() <= 0) {
+            $favorite = new Favorite;
+            $favorite->property_id = $house->property->id;
+            $favorite->user_id = auth()->id();
+            $favorite->house_id = $house->id;
+
+
+            try {
+                $favorite->save();
+                Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
                 return back();
-            }
-        }
-     
-        Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
-        return back();
+            } catch (\Illuminate\Database\QueryException $e) {
 
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == '1062') {
+                    Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+                    return back();
+                }
+            }
+
+            Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
+            return back();
+        } else {
+            Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            return back();
+        }
     }
 
-    public function favoriteLand(Land $land){
+    public function favoriteLand(Land $land)
+    {
 
-        $favorite = new Favorite;
-        $favorite->property_id = $land->property->id;
-        $favorite->user_id = auth()->id();
-        $favorite->land_id = $land->id;
-        
+        $isExits = Favorite::where('property_id', '=', $land->property->id)
+            ->where('user_id', '=', auth()->id())
+            ->get();
+        if ($isExits->count() <= 0) {
+            $favorite = new Favorite;
+            $favorite->property_id = $land->property->id;
+            $favorite->user_id = auth()->id();
+            $favorite->land_id = $land->id;
 
-        try{
-            $favorite->save();
-            Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
-            return back();
-        }catch(\Illuminate\Database\QueryException $e){
 
-            $errorCode = $e->errorInfo[1];
-            if($errorCode == '1062'){
-                Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            try {
+                $favorite->save();
+                Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
                 return back();
-            }
-        }
-     
-        Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
-        return back();
+            } catch (\Illuminate\Database\QueryException $e) {
 
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == '1062') {
+                    Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+                    return back();
+                }
+            }
+
+            Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
+            return back();
+        } else {
+            Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            return back();
+        }
     }
 
-    public function favoriteBuilding(Building $building){
+    public function favoriteBuilding(Building $building)
+    {
 
-        $favorite = new Favorite;
-        $favorite->property_id = $building->property->id;
-        $favorite->user_id = auth()->id();
-        $favorite->building_id = $building->id;
-        try{
-            $favorite->save();
-            Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
-            return back();
-        }catch(\Illuminate\Database\QueryException $e){
-
-            $errorCode = $e->errorInfo[1];
-            if($errorCode == '1062'){
-                Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+        $isExits = Favorite::where('property_id', '=', $building->property->id)
+            ->where('user_id', '=', auth()->id())
+            ->get();
+        if ($isExits->count() <= 0) {
+            $favorite = new Favorite;
+            $favorite->property_id = $building->property->id;
+            $favorite->user_id = auth()->id();
+            $favorite->building_id = $building->id;
+            try {
+                $favorite->save();
+                Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
                 return back();
-            }
-        }
-     
-        Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
-        return back();
+            } catch (\Illuminate\Database\QueryException $e) {
 
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == '1062') {
+                    Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+                    return back();
+                }
+            }
+
+            Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
+            return back();
+        } else {
+            Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            return back();
+        }
     }
 
-    public function favoriteApartment(Apartment $apartment){
+    public function favoriteApartment(Apartment $apartment)
+    {
 
-        // $alreadyFavorite = Favorite::where()
-        $favorite = new Favorite;
-        $favorite->property_id = $apartment->property->id;
-        $favorite->user_id = auth()->id();
-        $favorite->apartment_id = $apartment->id;
-        
-        try{
-            $favorite->save();
-            Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
-            return back();
-        }catch(\Illuminate\Database\QueryException $e){
+        $isExits = Favorite::where('property_id', '=', $apartment->property->id)
+            ->where('user_id', '=', auth()->id())
+            ->get();
+        if ($isExits->count() <= 0) {
+            $favorite = new Favorite;
+            $favorite->property_id = $apartment->property->id;
+            $favorite->user_id = auth()->id();
+            $favorite->apartment_id = $apartment->id;
 
-            $errorCode = $e->errorInfo[1];
-            if($errorCode == '1062'){
-                Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            try {
+                $favorite->save();
+                Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
                 return back();
-            }
-        }
-     
-        Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
-        return back();
+            } catch (\Illuminate\Database\QueryException $e) {
 
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == '1062') {
+                    Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+                    return back();
+                }
+            }
+
+            Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
+            return back();
+        } else {
+            Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            return back();
+        }
     }
 
-    public function favoriteWarehouse(Warehouse $warehouse){
+    public function favoriteWarehouse(Warehouse $warehouse)
+    {
 
-        // $alreadyFavorite = Favorite::where()
-        $favorite = new Favorite;
-        $favorite->property_id = $warehouse->property->id;
-        $favorite->user_id = auth()->id();
-        $favorite->warehouse_id = $warehouse->id;
-        
-        try{
-            $favorite->save();
-            Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
-            return back();
-        }catch(\Illuminate\Database\QueryException $e){
+        $isExits = Favorite::where('property_id', '=', $warehouse->property->id)
+            ->where('user_id', '=', auth()->id())
+            ->get();
+        if ($isExits->count() <= 0) {
+            $favorite = new Favorite;
+            $favorite->property_id = $warehouse->property->id;
+            $favorite->user_id = auth()->id();
+            $favorite->warehouse_id = $warehouse->id;
 
-            $errorCode = $e->errorInfo[1];
-            if($errorCode == '1062'){
-                Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            try {
+                $favorite->save();
+                Alert::success('Favorite has been added successfully!', 'Favorite Added')->autoclose(3000);
                 return back();
+            } catch (\Illuminate\Database\QueryException $e) {
+
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == '1062') {
+                    Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+                    return back();
+                }
             }
+
+            Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
+            return back();
+        } else {
+            Alert::warning('Favorite has been already added!', 'Already Added')->autoclose(3000);
+            return back();
         }
-     
-        Alert::error('Something went wrong!', 'Oops!')->autoclose(3000);
-        return back();
-
     }
-    
-
 }

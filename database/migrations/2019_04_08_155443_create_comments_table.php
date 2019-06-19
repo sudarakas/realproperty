dd60412027e56381ed39e25e1eaa39c8f3206b6e
sudarakas <sudharakafb@gmail.com>
2019-06-19 18:44:01 +0530

    comments table migration bug fixed

--- database/migrations/2019_04_08_155443_create_comments_table.php
+++ database/migrations/2019_04_08_155443_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('user_id');
-            $table->integer('post_id');
+            $table->integer('article_id');
             $table->string('comment');
             $table->timestamps();
         });

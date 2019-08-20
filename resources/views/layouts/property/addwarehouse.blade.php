87a2f9c67c51fb6852d22ccfb531284c755add28
sudarakas <sudharakafb@gmail.com>
2019-08-21 00:59:49 +0530

    addWarehouse error layout updated and contact no regex added

--- resources/views/layouts/property/addwarehouse.blade.php
+++ resources/views/layouts/property/addwarehouse.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="/css/bulma.min.css">
     <link rel="stylesheet" href="/css/custom.css">
     <link rel="stylesheet" href="/css/bootstrap.css"> {{-- Google Fonts --}}
-    <link href="https://fonts.googleapis.com/css?family=Exo+2:300i,400,400i,500,500i,600|Kanit:300,300i,400,400i,500,500i,600"
+    <link
+        href="https://fonts.googleapis.com/css?family=Exo+2:300i,400,400i,500,500i,600|Kanit:300,300i,400,400i,500,500i,600"
         rel="stylesheet">
     <style>
         #map {
@@ -28,7 +29,8 @@
     <div class="container">
         <div class="columns is-mobile is-centered">
             <div class="column is-8">
-    @include('layouts.errors') @if(session()->has('message'))
+                {{-- @include('layouts.errors') --}}
+                @if(session()->has('message'))
                 <div class="notification is-success">
                     <button class="delete"></button>
                     <h1 class="is-size-5"><b> {{ session()->get('message') }}</b></h1>
@@ -44,31 +46,46 @@
                     <div class="field">
                         <div class="control">
                             <label for="name">Property Name</label>
-                            <input class="input is-primary" type="text" name="name">
+                            <input class="input is-primary {{ $errors->has('name') ? ' is-danger' : '' }}" type="text"
+                                name="name" value="{{ old('name') }}">
+                            @if ($errors->has('name'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('name') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Property Type</label>
                             <br>
-                            <div class="select is-primary is-full">
+                            <div class="select is-primary is-full {{ $errors->has('type') ? ' is-danger' : '' }}">
                                 <select name="type">
                                     <option value="Warehouse">Warehouse</option>
                                 </select>
+                                @if ($errors->has('type'))
+                                <span>
+                                    <strong class="has-text-danger">{{ $errors->first('type') }}</strong>
+                                </span> @endif
                             </div>
+
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Property Price <strong>(Rs.)</strong></label>
-                            <input class="input is-primary" type="text" name="amount">
+                            <input class="input is-primary {{ $errors->has('amount') ? ' is-danger' : '' }}" type="text"
+                                name="amount" value="{{ old('amount') }}">
+                            @if ($errors->has('amount'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('amount') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">City</label>
                             <br>
-                            <div class="select is-primary is-full">
+                            <div class="select is-primary is-full {{ $errors->has('name') ? ' is-danger' : '' }}">
                                 <select name="city">
                                     <option>Ambalangoda</option>
                                     <option>Ampara</option>
@@ -130,32 +147,37 @@
                                     <option>Wattala</option>
                                     <option>Wattegama</option>
                                     <option>Weligama</option>
-                        </select>
+                                </select>
                             </div>
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Postal Code</label>
-                            <input class="input is-primary" type="text" name="postalcode">
+                            <input class="input is-primary {{ $errors->has('postalcode') ? ' is-danger' : '' }}" type="text"
+                                name="postalcode" value="{{ old('postalcode') }}">
+                            @if ($errors->has('postalcode'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('postalcode') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Province</label>
                             <br>
-                            <div class="select is-primary is-full">
+                            <div class="select is-primary is-full {{ $errors->has('province') ? ' is-danger' : '' }}">
                                 <select name="province">
-                                        <option value="Central">Central</option>
-                                        <option value="Eastern">Eastern</option>
-                                        <option value="NorthCentral">North Central</option>
-                                        <option value="NorthWestern">North Western</option>
-                                        <option value="Northern">Northern</option>
-                                        <option value="Sabaragamuwa">Sabaragamuwa</option>
-                                        <option value="Southern">Southern</option>
-                                        <option value="Uva">Uva</option>
-                                        <option value="Western">Western</option>
-                                    </select>
+                                    <option value="Central">Central</option>
+                                    <option value="Eastern">Eastern</option>
+                                    <option value="NorthCentral">North Central</option>
+                                    <option value="NorthWestern">North Western</option>
+                                    <option value="Northern">Northern</option>
+                                    <option value="Sabaragamuwa">Sabaragamuwa</option>
+                                    <option value="Southern">Southern</option>
+                                    <option value="Uva">Uva</option>
+                                    <option value="Western">Western</option>
+                                </select>
                             </div>
                         </div>
                     </div>
@@ -163,29 +185,47 @@
                     <div class="field">
                         <div class="control">
                             <label for="name">Property Description</label>
-                            <textarea name="description" class="textarea is-primary"></textarea>
+                            <textarea name="description"
+                                class="textarea is-primary {{ $errors->has('description') ? ' is-danger' : '' }}"
+                                value="{{ old('description') }}"></textarea>
+                            @if ($errors->has('description'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('description') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Contact No</label>
-                            <input class="input is-primary" type="text" name="contactno">
+                            <input class="input is-primary {{ $errors->has('contactno') ? ' is-danger' : '' }}" type="text"
+                                name="contactno" value="{{ old('contactno') }}">
+                            @if ($errors->has('contactno'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('contactno') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Contact Email</label>
-                            <input class="input is-primary" type="text" name="contactemail" value="{{Auth::user()->email}}">
+                            <input class="input is-primary {{ $errors->has('contactemail') ? ' is-danger' : '' }}" type="text"
+                                name="contactemail" value="{{Auth::user()->email}}">
+                            @if ($errors->has('contactemail'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('contactemail') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     {{-- Image Upload Section --}}
                     <div class="field">
                         <div class="control">
-                            <label for="img">Images <strong class="is-small">(Tip: Upload more the one photograph [Max Image Size: 4MB])</strong></label>
+                            <label for="img">Images <strong class="is-small">(Tip: Upload more the one photograph [Max
+                                    Image Size: 4MB])</strong></label>
                             <div class="input-group control-group increment">
                                 <input type="file" name="filename[]" class="form-control">
                                 <div class="input-group-btn">
-                                    <button class="button is-success addmore" type="button"><i class="glyphicon glyphicon-plus"></i>More</button>
+                                    <button class="button is-success addmore" type="button"><i
+                                            class="glyphicon glyphicon-plus"></i>More</button>
                                 </div>
                             </div>
 
@@ -193,11 +233,16 @@
                                 <div class="control-group input-group" style="margin-top:10px">
                                     <input type="file" name="filename[]" class="form-control">
                                     <div class="input-group-btn">
-                                        <button class="button is-danger" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
+                                        <button class="button is-danger" type="button"><i
+                                                class="glyphicon glyphicon-remove"></i> Remove</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        @if ($errors->has('filename'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('filename') }}</strong>
+                            </span> @endif
                     </div>
                     <div class="field">
                         <div class="control maphere">
@@ -209,13 +254,23 @@
                     <div class="field">
                         <div class="control">
                             <label for="name">Latitude</label>
-                            <input class="input is-primary" type="text" name="lat" id="lat">
+                            <input class="input is-primary {{ $errors->has('lat') ? ' is-danger' : '' }}" type="text"
+                                name="lat" id="lat" value="{{ old('lat') }}">
+                            @if ($errors->has('lat'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('name') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Longitude</label>
-                            <input class="input is-primary" type="text" name="lng" id="lng">
+                            <input class="input is-primary {{ $errors->has('lng') ? ' is-danger' : '' }}" type="text"
+                                name="lng" id="lng" value="{{ old('lng') }}">
+                            @if ($errors->has('lng'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('lng') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                 </div>
@@ -225,7 +280,7 @@
                         <div class="control">
                             <label for="name">Agreement Type</label>
                             <br>
-                            <div class="select is-primary">
+                            <div class="select is-primary {{ $errors->has('agreement') ? ' is-danger' : '' }}">
                                 <select name="agreement">
                                     <option value="Sale">Sale</option>
                                     <option value="Lease" selected>Lease</option>
@@ -237,7 +292,7 @@
                         <div class="control">
                             <label for="electricity">Electricity</label>
                             <br>
-                            <div class="select is-primary">
+                            <div class="select is-primary {{ $errors->has('electricity') ? ' is-danger' : '' }}">
                                 <select name="electricity">
                                     <option>Not Available</option>
                                     <option>2 Phase</option>
@@ -250,27 +305,32 @@
                         <div class="control">
                             <label for="name">Parking Area</label>
                             <br>
-                            <div class="select is-primary">
+                            <div class="select is-primary {{ $errors->has('carpark') ? ' is-danger' : '' }}">
                                 <select name="carpark">
-                                        <option value="Available">Available</option>
-                                        <option value="Not Available" selected>Not Available</option>
-                                    </select>
+                                    <option value="Available">Available</option>
+                                    <option value="Not Available" selected>Not Available</option>
+                                </select>
                             </div>
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
                             <label for="name">Size(Square Feet)</label>
-                            <input class="input is-primary" type="number" name="size">
+                            <input class="input is-primary {{ $errors->has('size') ? ' is-danger' : '' }}" type="number"
+                                name="size" value="{{ old('size') }}">
+                            @if ($errors->has('size'))
+                            <span>
+                                <strong class="has-text-danger">{{ $errors->first('size') }}</strong>
+                            </span> @endif
                         </div>
                     </div>
                     <div class="field">
                         <div class="control is-pulled-right">
                             <button type="submit" class="button is-primary">
-                               Add Warehouse
+                                Add Warehouse
                             </button>
                             <button type="reset" class="button is-warning">
-                                    Clear
+                                Clear
                             </button>
                         </div>
                     </div>
@@ -332,7 +392,8 @@
                 });
             }
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKNG_uMsCgUvpLc_Adr2n9nwo6BWOImoM&libraries=places&callback=initAutocomplete"
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKNG_uMsCgUvpLc_Adr2n9nwo6BWOImoM&libraries=places&callback=initAutocomplete"
         async defer></script>
     <script type="text/javascript">
         $(document).ready(function() {

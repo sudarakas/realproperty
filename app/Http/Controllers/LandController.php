8a479fd13fd9e98ab9eb29a54f3cfcc9044d2c9e
sudarakas <sudharakafb@gmail.com>
2019-08-30 20:03:35 +0530

    deleteland email notitification

--- app/Http/Controllers/LandController.php
+++ app/Http/Controllers/LandController.php
@@ -179,6 +179,20 @@ class LandController extends Controller
             DB::table('lands')->where('id', '=', $land->id)->delete();
             DB::table('properties')->where('id', '=', $land->property->id)->delete();
 
+            if (Auth::guard('admin')->check()) {
+
+                $message = new MailNotification;
+                $message->receiver_email = $property->user->email;
+                $message->receiver_name = $property->user->name;
+                $message->property_name = $property->name;
+                $message->property_location = $property->city;
+                $message->property_createdOn = $property->created_at;
+                $message->status = 'deleted';
+                $message->subject = "Your property has been deleted!";
+
+                \Mail::to($message->receiver_email)->send(new EmailNotification($message));
+            }
+            
             Alert::success('Your property has been deleted successfully!', 'Successfully Deleted!')->autoclose(3000);
             return back();
         }
